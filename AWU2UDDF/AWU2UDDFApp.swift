00a304213d755726e3a0748813295dfbf26690ed
Doug Junkins <junkins@foghead.com>
2022-12-26 21:09:11 -0800

    Initial Commit

---
+++ AWU2UDDF/AWU2UDDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWU2UDDFApp.swift
+//  AWU2UDDF
+//
+//  Created by Doug Junkins on 12/26/22.
+//
+
+import SwiftUI
+
+@main
+struct AWU2UDDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AWU2UDDF/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AWU2UDDF
+//
+//  Created by Doug Junkins on 12/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
